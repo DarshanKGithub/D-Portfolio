bc4c26c3a04bb324de05abac802b4137082c4230
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-06-24 10:16:26 +0530

    Add files via upload

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -6,7 +6,7 @@ function Heading() {
     <div className="relative flex flex-col items-center justify-center min-h-screen">
       <div className="absolute top-1/4 left-4 md:left-10 text-white text-center md:text-left">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
-          <span className="absolute text-6xl  left-0">I'm</span> <br />
+          <span className="absolute text-6xl left-0">I'm</span> <br />
           <span className="relative text-red-500">Darshan</span> Kshetri
         </h1>
         <p className="text-sm md:text-base lg:text-lg text-purple-400/100 hover:text-pink-300 cursor-none">
@@ -14,21 +14,22 @@ function Heading() {
         </p>
       </div>
       <div className="absolute left-5 md:left-10 w-11/12 md:w-3/4 lg:w-1/2 text-white text-xs md:text-sm lg:text-base text-start">
-        <p className="relative font-semibold mt-24 hover:text-emerald-600">
+        <p className="relative font-semibold mt-24 md:mt-16 lg:mt-24 hover:text-emerald-600">
           Aspiring web developer with a strong foundation in front-end
           technologies, seeking
-          <br /> to leverage my skills in HTML, CSS, JavaScript, and React.js to
-          contribute to dynamic
-          <br /> web development projects. Eager to learn and grow in a
-          collaborative environment
-          <br /> while delivering high-quality web solutions.
+          <br className="hidden md:block" /> to leverage my skills in HTML, CSS,
+          JavaScript, and React.js to contribute to dynamic
+          <br className="hidden md:block" /> web development projects. Eager to
+          learn and grow in a collaborative environment
+          <br className="hidden md:block" /> while delivering high-quality web
+          solutions.
         </p>
       </div>
-      <div className="absolute bottom-2 md:top-2/2 md:right-10 transform md:-translate-y-1/2 drop-shadow-2xl">
+      <div className="absolute bottom-2 md:top-1/2 md:right-10 transform md:-translate-y-1/2 drop-shadow-2xl">
         <img
           src={image}
           alt="Darshan KShetri"
-          className="w-24 h-24 sm:w-36 sm:h-36 md:w-48 md:h-48 lg:w-72 lg:h-72 object-cover rounded-full"
+          className="w-32 h-32 sm:w-44 sm:h-44 md:w-56 md:h-56 lg:w-80 lg:h-80 xl:w-96 xl:h-96 object-cover rounded-full"
         />
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar({ scrollToContact }) {
-  const [isOpen, setIsOpen] = useState(false); 
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -11,58 +11,57 @@ function Navbar({ scrollToContact }) {
 
   const handleClick = () => {
     navigate("/");
-  }
-
-  
+  };
 
   return (
     <div className="text-white">
-      <div className="bg-black p-5 flex justify-center items-center">
-        <Link className="text-lg font-bold flex " onClick={handleClick}>DKODER</Link>
-        <div className="md:hidden absolute flexbox left-3">
+      <div className="bg-black p-5 flex justify-between items-center">
+        <Link className="text-lg font-bold" onClick={handleClick}>
+          DKODER
+        </Link>
+        <div className="md:hidden">
           <button
             onClick={toggleMenu}
             className="text-white focus:outline-none"
           >
-            {isOpen ? <FaTimes size={20} /> : <FaBars size={20} />}
+            {isOpen ? <FaTimes size={22} /> : <FaBars size={22} />}
           </button>
         </div>
         <div
-          className={`flex-col md:flex md:flex-row md:items-center relative md:static right-11 top-2  md:bg-transparent w-full md:w-auto transition-all duration-300 ease-in-out ${
-            isOpen ? "top-16 opacity-100" : "top-[-490px] opacity-0"
-          } md:opacity-100`}
+          className={`absolute md:relative md:flex md:flex-row md:items-center bg-black md:bg-transparent w-full md:w-auto transition-all duration-300 ease-in-out ${
+            isOpen ? "top-16 opacity-100 z-50" : "top-[-490px] opacity-0 z-[-1]"
+          } md:opacity-100 md:top-0`}
         >
           <Link
-            className="hover:text-red-900 p-2 md:p-0 md:ml-4"
+            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4"
             to="/"
             onClick={() => setIsOpen(false)}
           >
             Home
           </Link>
           <Link
-            className="hover:text-red-900 p-2 md:p-0 md:ml-4"
+            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4"
             to="/about"
             onClick={() => setIsOpen(false)}
           >
             About
           </Link>
-        
           <Link
-            className="hover:text-red-900 p-2 md:p-0 md:ml-4"
+            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4"
             to="/projects"
             onClick={() => setIsOpen(false)}
           >
             Projects
           </Link>
           <Link
-            className="hover:text-red-900 p-2 md:p-0 md:ml-4"
+            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4"
             to="/resume"
             onClick={() => setIsOpen(false)}
           >
             Resume
           </Link>
-          <span href="/contacts"
-            className="hover:text-red-900 p-2 md:p-0 md:ml-4 cursor-pointer"
+          <span
+            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4 cursor-pointer"
             onClick={() => {
               scrollToContact();
               setIsOpen(false);
