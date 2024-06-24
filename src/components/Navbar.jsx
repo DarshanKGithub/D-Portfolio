7808ed73322d03625be8ca1ad513562a6d7207cc
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-06-24 10:37:10 +0530

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,53 +15,58 @@ function Navbar({ scrollToContact }) {
 
   return (
     <div className="text-white">
-      <div className="bg-black p-5 flex justify-between items-center">
-        <Link className="text-lg font-bold" onClick={handleClick}>
+      <div className="bg-black p-5 flex justify-center items-center">
+        <Link
+          className="text-lg font-bold flex relative left-"
+          onClick={handleClick}
+        >
           DKODER
         </Link>
-        <div className="md:hidden">
+        <div className="md:hidden absolute flexbox left-3">
           <button
             onClick={toggleMenu}
             className="text-white focus:outline-none"
           >
-            {isOpen ? <FaTimes size={22} /> : <FaBars size={22} />}
+            {isOpen ? <FaTimes size={20} /> : <FaBars size={20} />}
           </button>
         </div>
         <div
-          className={`absolute md:relative md:flex md:flex-row md:items-center bg-black md:bg-transparent w-full md:w-auto transition-all duration-300 ease-in-out ${
-            isOpen ? "top-16 opacity-100 z-50" : "top-[-490px] opacity-0 z-[-1]"
-          } md:opacity-100 md:top-0`}
+          className={`flex-col md:flex md:flex-row md:items-center relative md:static right-11 top-111 md:bg-transparent w-full md:w-auto transition-all duration-300 ease-in-out ${
+            isOpen ? "top-16 opacity-100" : "top-[-490px] opacity-0"
+          } md:opacity-100`}
         >
           <Link
-            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4"
+            className="hover:text-red-900 p-2 md:p-0 md:ml-4"
             to="/"
             onClick={() => setIsOpen(false)}
           >
             Home
           </Link>
           <Link
-            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4"
+            className="hover:text-red-900 p-2 md:p-0 md:ml-4"
             to="/about"
             onClick={() => setIsOpen(false)}
           >
             About
           </Link>
+
           <Link
-            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4"
+            className="hover:text-red-900 p-2 md:p-0 md:ml-4"
             to="/projects"
             onClick={() => setIsOpen(false)}
           >
             Projects
           </Link>
           <Link
-            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4"
+            className="hover:text-red-900 p-2 md:p-0 md:ml-4"
             to="/resume"
             onClick={() => setIsOpen(false)}
           >
             Resume
           </Link>
           <span
-            className="block md:inline-block hover:text-red-900 p-2 md:p-0 md:ml-4 cursor-pointer"
+            href="/contacts"
+            className="hover:text-red-900 p-2 md:p-0 md:ml-4 cursor-pointer"
             onClick={() => {
               scrollToContact();
               setIsOpen(false);
