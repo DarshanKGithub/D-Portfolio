1e8c1fa436f88c0f966e0c2037be03eb56a8ba9a
DarshanKGithub <kshetridarshan993@gmail.com>
2025-02-25 17:23:26 +0530

    initial commit

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <div className="flex justify-center items-center h-screen bg-cover bg-center bg-fixed backdrop-blur-sm">
       <a
-        href="/Darshan_Kshetri.pdf"
+        href="/Darshan_Kshetri_Resume.pdf"
         download
         className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700"
       >
